effa744ca1ec02bec4e09fca03ee4f1045959139
Anand <anandyadav.8501@.com>
2023-08-13 00:22:03 +0530

    fixedcallback

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
+  //callbackURL: "http://localhost:3000/auth/google/secrets",
+  callbackURL: "https://secrets-xqg0.onrender.com/auth/google/secrets",
   serProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(request, accessToken, refreshToken, profile, cb) {
